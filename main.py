ea2a3bbe227c9f4d113d99374ba82a735bd207e6
aidyn <aidynsemey7sk@gmail.com>
2022-03-06 16:58:21 +0600

    My commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+from pyfiglet import Figlet
+import folium
+
+
+def get_info_by_ip(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+        data = {
+            '[IP]': response.get('query'),
+            '[Int prov]': response.get('isp'),
+            '[Org]': response.get('org'),
+            '[Region name]': response.get('regionName'),
+            '[Country]': response.get('country'),
+            '[City]': response.get('city'),
+            '[ZIP]': response.get('zip'),
+            '[Lat]': response.get('lat'),
+            '[Lon]': response.get('lon'),
+        }
+
+        for k, v in data.items():
+            print(f'{k} : {v}')
+
+        area = folium.Map(location=[response.get(
+            'lat'), response.get('lon')], zoom_start=13)
+        folium.Marker(
+            [response.get('lat'), response.get('lon')], popup="<i>Mt. Hood Meadows</i>", tooltip="Этот пёс находится тут"
+        ).add_to(area)
+        area.save(f"{response.get('query')}_{response.get('city')}.html")
+
+    except requests.exceptions.ConnectionError:
+        print('[!] Please check your connection!')
+
+
+def main():
+    preview_text = Figlet(font='slant')
+    print(preview_text.renderText('COORDINATES RECEIVED'))
+    ip = input('Please enter a target IP: ')
+
+    get_info_by_ip(ip=ip)
+
+
+if __name__ == '__main__':
+    main()
